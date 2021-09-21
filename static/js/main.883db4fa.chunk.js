8a327b1ad4ecbe4aca7b5c5169c9f6fe5ce06b92
mashtalerleonid <mashtalerleonid@gmail.com>
2021-09-21 21:44:46 +0300

    Updates

---
+++ static/js/main.883db4fa.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-05-movies"]=this["webpackJsonpgoit-react-hw-05-movies"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var c=n(1),i=n.n(c),o=n(3),r=n.n(o),s=(n(8),n(9),n(0));var a=function(){return Object(s.jsx)("div",{})};r.a.render(Object(s.jsx)(i.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.883db4fa.chunk.js.map
